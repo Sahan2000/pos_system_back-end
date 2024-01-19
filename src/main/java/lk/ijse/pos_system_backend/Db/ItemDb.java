3f6ce622278786bfd5f19350edf69e80a0244273
Sahan2000 <udanasahan1@gmail.com>
2024-01-19 15:04:32 +0530

    complete crud operation item form

--- src/main/java/lk/ijse/pos_system_backend/Db/ItemDb.java
+++ src/main/java/lk/ijse/pos_system_backend/Db/ItemDb.java
@@ -1,5 +1,6 @@
 package lk.ijse.pos_system_backend.Db;
 
+import lk.ijse.pos_system_backend.api.Item;
 import lk.ijse.pos_system_backend.dto.CustomerDTO;
 import lk.ijse.pos_system_backend.dto.ItemDTO;
 
@@ -7,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ItemDb {
     public String generateItemCode(Connection connection){
@@ -47,5 +49,77 @@ public class ItemDb {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public ArrayList<ItemDTO> getAllItem(Connection connection){
+        String sql = "select * from item;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ArrayList<ItemDTO> itemDTOS = new ArrayList<>();
+            while (resultSet.next()){
+                ItemDTO itemDTO = new ItemDTO(
+                        resultSet.getString("itemCode"),
+                        resultSet.getString("description"),
+                        resultSet.getString("qty"),
+                        resultSet.getString("price")
+                );
+                itemDTOS.add(itemDTO);
+            }
+            return itemDTOS;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteItem(Connection connection, String code){
+        String sql = "delete from item where itemCode=?;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,code);
+            return preparedStatement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean updateItem(Connection connection, ItemDTO itemDTO){
+        String sql = "update item set description=?, qty=?, price=? where itemCode=?;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,itemDTO.getDescription());
+            preparedStatement.setString(2,itemDTO.getQty());
+            preparedStatement.setString(3,itemDTO.getPrice());
+            preparedStatement.setString(4,itemDTO.getItemCode());
+
+            return preparedStatement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ItemDTO getItem(Connection connection, String code){
+        String sql = "select * from item where itemCode=?;";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,code);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                return new ItemDTO(
+                        resultSet.getString("itemCode"),
+                        resultSet.getString("description"),
+                        resultSet.getString("qty"),
+                        resultSet.getString("price")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
--- src/main/java/lk/ijse/pos_system_backend/api/Item.java
+++ src/main/java/lk/ijse/pos_system_backend/api/Item.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.pos_system_backend.Db.CustomerDb;
 import lk.ijse.pos_system_backend.Db.ItemDb;
+import lk.ijse.pos_system_backend.dto.CustomerDTO;
 import lk.ijse.pos_system_backend.dto.ItemDTO;
 
 import javax.naming.InitialContext;
@@ -17,6 +18,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet(name = "item", urlPatterns = "/item")
 public class Item extends HttpServlet {
@@ -37,6 +39,11 @@ public class Item extends HttpServlet {
         String action = req.getParameter("action");
         if (action.equals("generateItemCode")){
             generateItemCode(req,resp);
+        }else if (action.equals("getAllItem")){
+            getAllItem(req,resp);
+        } else if (action.equals("getItem")) {
+            String code = req.getParameter("itemCode");
+            getItem(req,resp,code);
         }
     }
 
@@ -72,6 +79,72 @@ public class Item extends HttpServlet {
             }
         }else {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getContentType() != null && req.getContentType().toLowerCase().startsWith("application/json")){
+            Jsonb jsonb = JsonbBuilder.create();
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+
+            var itemDb = new ItemDb();
+            boolean result = itemDb.updateItem(connection, itemDTO);
+
+            if (result){
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().write("Customer information updated successfully!");
+            }else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Failed to saved customer information!");
+            }
+        }else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("itemCode");
+        System.out.println(code);
+        var itemDb = new ItemDb();
+        boolean result = itemDb.deleteItem(connection, code);
+
+        if (result){
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write("Customer information deleted successfully!");
+        }else {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Failed to saved customer information!");
+        }
+    }
+
+    private void getAllItem(HttpServletRequest req, HttpServletResponse resp){
+        var itemDb = new ItemDb();
+        ArrayList<ItemDTO> allItem = itemDb.getAllItem(connection);
+
+        Jsonb jsonb = JsonbBuilder.create();
+        var json = jsonb.toJson(allItem);
+
+        resp.setContentType("application/json");
+        try {
+            resp.getWriter().write(json);
+        } catch (IOException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getItem(HttpServletRequest req, HttpServletResponse resp, String code){
+        var itemDb = new ItemDb();
+        ItemDTO itemDTO = itemDb.getItem(connection, code);
+        Jsonb jsonb = JsonbBuilder.create();
+
+        var json = jsonb.toJson(itemDTO);
+        resp.setContentType("application/json");
+        try {
+            resp.getWriter().write(json);
+        } catch (IOException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
         }
     }
 }
